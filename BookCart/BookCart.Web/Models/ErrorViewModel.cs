4515dc8b173cec715627cec4deae40b3a5bcc99d
loveleensingh97 <loveleensingh1997@gmail.com>
2024-06-15 16:29:52 +0530

    Initial Commit

---
+++ BookCart/BookCart.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCart.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
